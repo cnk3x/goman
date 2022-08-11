95674f5ba5fb6430adcd1b8021ec48a80f5d61fc
wen <wen@w7x.cn>
2022-08-11 16:39:35 +0800

    -

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 curl -sLo /usr/local/bin/goman https://raw.githubusercontent.com/cnk3x/goman/main/goman
-sed -i 's!https://golang.google.cn/dl!https://go.dev/dl/!g; s!https://gitee.com/mirrors/go.git!https://github.com/golang/go.git!g' /usr/local/bin/goman
+sed -i 's!https://golang.google.cn/dl!https://go.dev/dl!g; s!https://gitee.com/mirrors/go.git!https://github.com/golang/go.git!g' /usr/local/bin/goman
 chmod +x /usr/local/bin/goman
 goman
